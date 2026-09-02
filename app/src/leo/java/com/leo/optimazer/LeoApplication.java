7719fa3fda600f86b68bf8236c95a9c00b0982bc
joaobittor05-byte <joaobittor05@gmail.com>
2026-09-01 22:39:42 -0300

    Leo Optimazer: inicializar Shizuku como núcleo principal

--- app/src/leo/java/com/leo/optimazer/LeoApplication.java
+++ app/src/leo/java/com/leo/optimazer/LeoApplication.java
@@ -6,6 +6,6 @@ public class LeoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        BridgeClient.initialize(this);
+        ShizukuCore.initialize(this);
     }
 }
